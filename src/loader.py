177a6d38758a67742c7b42f5fcbd2391ca70eb25
hugoarr1975 <39355609+hugoarr1975@users.noreply.github.com>
2026-07-30 16:39:24 -0500

    loader.py

--- src/loader.py
+++ src/loader.py
@@ -1,8 +1,17 @@
 import pandas as pd
+import streamlit as st
 
+
+@st.cache_data
 def load_data():
 
-    df = pd.read_csv("data/apartamentos_akila.csv")
+    df = pd.read_csv(
+        "data/apartamentos_akila.csv"
+    )
+
+    # ------------------------
+    # Fechas
+    # ------------------------
 
     df["fecha_venta"] = pd.to_datetime(
         df["fecha_venta"],
@@ -12,6 +21,57 @@ def load_data():
     df["fecha_entrega"] = pd.to_datetime(
         df["fecha_entrega"],
         errors="coerce"
+    )
+
+    # ------------------------
+    # Valores nulos
+    # ------------------------
+
+    columnas_texto = [
+        "estado",
+        "forma_pago",
+        "tipo_apartamento",
+        "torre"
+    ]
+
+    for c in columnas_texto:
+        df[c] = df[c].fillna("No aplica")
+
+    columnas_numero = [
+        "precio_cop",
+        "area_m2",
+        "monto_credito_cop",
+        "monto_contado_cop",
+        "porcentaje_credito"
+    ]
+
+    for c in columnas_numero:
+        df[c] = df[c].fillna(0)
+
+    # ------------------------
+    # Eliminar duplicados
+    # ------------------------
+
+    df = df.drop_duplicates()
+
+    # ------------------------
+    # Columnas auxiliares
+    # ------------------------
+
+    df["Semana"] = (
+        df["fecha_venta"]
+        .dt.to_period("W")
+        .astype(str)
+    )
+
+    df["Mes"] = (
+        df["fecha_venta"]
+        .dt.strftime("%Y-%m")
+    )
+
+    df["Año"] = (
+        df["fecha_venta"]
+        .dt.year
     )
 
     return df
