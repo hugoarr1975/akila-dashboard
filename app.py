ff1b424e0b7080c8d261e1cc613f1e8083097077
hugoarr1975 <39355609+hugoarr1975@users.noreply.github.com>
2026-08-01 19:32:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,14 @@
-from src.loader import load_data
-
-from src.filters import sidebar_filters
-
-from src.dashboard import render_dashboard
-
-from src.styles import load_css
-
+# -*- coding: utf-8 -*-
 import streamlit as st
+from src.loader import load_data
+from src.filters import sidebar_filters
+from src.dashboard import render_dashboard
+from src.styles import load_css, page_header
 
-st.set_page_config(
-
-    page_title="AKILA",
-
-    layout="wide"
-
-)
-
+st.set_page_config( page_title="AKILA Dashboard",
+                    page_icon="🏢", layout="wide")
 load_css()
-
+page_header()
 df = load_data()
-
 df = sidebar_filters(df)
-
 render_dashboard(df)
